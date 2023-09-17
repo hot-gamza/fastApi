6ed16e605e89b1ca1b2dd7f628b50a80b645d7ef
InchaeNa <dlsco1230@gmail.com>
2023-09-17 10:37:21 +0900

    "prototype" able to download with name

--- routers/main_routes.py
+++ routers/main_routes.py
@@ -25,16 +25,25 @@ async def index(imgFile: List[UploadFile] = File(...)):
     if not result_filepath:
         raise HTTPException(status_code=400, detail="Face swapping failed")
     
-    # 이미지를 PIL.Image 객체로 로드
-    image = Image.open(result_filepath)
+    # # 이미지를 PIL.Image 객체로 로드
+    # image = Image.open(result_filepath)
     
-    # 이미지를 바이트 스트림으로 변환
-    image_stream = BytesIO()
-    image.save(image_stream, format='JPEG')
-    image_stream.seek(0)
+    # # 이미지를 바이트 스트림으로 변환
+    # image_stream = BytesIO()
+    # image.save(image_stream, format='JPEG')
+    # image_stream.seek(0)
     
-    # 스트리밍 응답으로 반환
-    return StreamingResponse(image_stream, media_type="image/jpeg")
+    # # 스트리밍 응답으로 반환
+    # return StreamingResponse(image_stream, media_type="image/jpeg")
+
+    template_img = template_img_path
+    result_filepath_list  = await faceswap(template_img, male_filename, female_filename)
+    result_filepath = result_filepath_list[0]
+
+    # 처리된 이미지 파일을 반환
+    return FileResponse(result_filepath, headers={"Content-Disposition": f"attachment; filename={os.path.basename(result_filepath)}"})
+
+
 
 
 
